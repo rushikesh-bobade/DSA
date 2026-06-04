c6d4255a479bbb773337dbfde67157018a2a19cd
Rushikesh Bobade <155564428+rushikesh-bobade@users.noreply.github.com>
2026-06-05 02:29:03 +0530

    ✅ Move Zeroes | Easy | Java | Runtime: 2 ms (91.65%) | Memory: 47.6 MB | 2026-06-04

--- easy/0283-move-zeroes/solution.java
+++ easy/0283-move-zeroes/solution.java
@@ -1,4 +1,27 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        
+    int left=-1;
 
-        for(int i=left+1;i<nums.length;i++){
-            if(nums[i]!=0){
-                int temp=nums[i];
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==0){
+                left=i;
+                break;
+            }
+        }
+
+        if(left==-1){
+            return;
+        }
+
+        for(int i=left+1;i<nums.length;i++){
+            if(nums[i]!=0){
+                int temp=nums[i];
+                nums[i]=nums[left];
+                nums[left]=temp;
+                left++;
+            }
+
+        }
+    }
+}

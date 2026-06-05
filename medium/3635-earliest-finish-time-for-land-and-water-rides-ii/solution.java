2bdc0874cf74ffbf3cead8a51e55ea3fb0874afd
Rushikesh Bobade <155564428+rushikesh-bobade@users.noreply.github.com>
2026-06-05 11:38:16 +0530

    ✅ Earliest Finish Time for Land and Water Rides II | Medium | Java | Runtime: 3 ms | Memory: 42.8 MB | 2026-06-05

---
+++ medium/3635-earliest-finish-time-for-land-and-water-rides-ii/solution.java
@@ -0,0 +1,19 @@
+        int m = secondStart.length;
+
+        int[][] rides = new int[m][2];
+        for (int i = 0; i < m; i++) {
+            rides[i][0] = secondStart[i];
+    private long solve(int[] firstStart, int[] firstDur, int[] secondStart, int[] secondDur) {
+        int n = firstStart.length;
+
+    }
+        );
+        return (int) ans;
+            solve(landStartTime, landDuration, waterStartTime, waterDuration),
+            solve(waterStartTime, waterDuration, landStartTime, landDuration)
+class Solution {
+    public int earliestFinishTime(int[] landStartTime, int[] landDuration, int[] waterStartTime, int[] 
+    waterDuration) {
+        long ans = Math.min(
+import java.util.*;
+

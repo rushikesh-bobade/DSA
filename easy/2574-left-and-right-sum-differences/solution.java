8435f8e824912d12a1e44166db094f33ef9d5baf
Rushikesh Bobade <155564428+rushikesh-bobade@users.noreply.github.com>
2026-06-07 01:14:45 +0530

    ✅ Left and Right Sum Differences | Easy | Java | Runtime: 0 ms | Memory: 42.9 MB | 2026-06-06

--- easy/2574-left-and-right-sum-differences/solution.java
+++ easy/2574-left-and-right-sum-differences/solution.java
@@ -1,13 +1,8 @@
-       leftSum[0]=0;
-       for(int i=1;i<nums.length;i++){
-        leftSum[i]=leftSum[i-1]+nums[i];
-       }
-
        int rightSum=0;
-       for(int i=nums.length-1;i>=0;i--){
-       }
+       for(int i=nums.length-1;i>0;i--){
         leftSum[i]=leftSum[i]+rightSum;
         rightSum=rightSum+nums[i];
+       }
 
-       return leftSum;
-    }
+       }
+        leftSum[i]=leftSum[i-1]+nums[i];

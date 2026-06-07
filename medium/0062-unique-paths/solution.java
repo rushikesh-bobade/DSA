c6041c4baf02309f872e338d0ab96c4cd8722103
Rushikesh Bobade <155564428+rushikesh-bobade@users.noreply.github.com>
2026-06-07 12:20:20 +0530

    ✅ Unique Paths | Medium | Java | Runtime: 0 ms | Memory: 41.9 MB | 2026-06-07

--- medium/0062-unique-paths/solution.java
+++ medium/0062-unique-paths/solution.java
@@ -1,3 +1,10 @@
+            return 1;
+        if(i==m-1||j==n-1){
+        
+    public int grid(int i,int j,int m,int n){
+        }else if(i==m||j==n){
+            return 0;
+        }
 
         int w1=grid(i+1,j,m,n);
         int w2=grid(i,j+1,m,n);
@@ -6,12 +13,4 @@
     }
 
     public int uniquePaths(int m, int n) {  
-        }
-            return 0;
-        }else if(i==m||j==n){
-        if(i==n-1||j==n-1){
-            return 1;
-    public int grid(int i,int j,int m,int n){
-        
-
-class Solution {
+       int ans= grid(0,0,m,n);

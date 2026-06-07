93bb8db1287f681dafeaeffe2e04c2ccf2112cb3
Rushikesh Bobade <155564428+rushikesh-bobade@users.noreply.github.com>
2026-06-07 11:40:27 +0530

    ✅ Create Binary Tree From Descriptions | Medium | Java | Runtime: 66 ms (73.78%) | Memory: 86.2 MB | 2026-06-07

--- medium/2196-create-binary-tree-from-descriptions/solution.java
+++ medium/2196-create-binary-tree-from-descriptions/solution.java
@@ -1,16 +1,44 @@
 /**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-class Solution {
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode createBinaryTree(int[][] descriptions) {
+        Map<Integer, TreeNode> map = new HashMap<>();
+        Set<Integer> children = new HashSet<>();
+
+        for (int[] d : descriptions) {
+            int parent = d[0], child = d[1], isLeft = d[2];
+
+            map.putIfAbsent(parent, new TreeNode(parent));
+            map.putIfAbsent(child, new TreeNode(child));
+
+            if (isLeft == 1) {
+                map.get(parent).left = map.get(child);
+            } else {
+                map.get(parent).right = map.get(child);
+            }
+
+            children.add(child);
+        }
+
+        for (int[] d : descriptions) {
+            if (!children.contains(d[0])) {
+                return map.get(d[0]);
+            }
+        }
+
+        return null;
+    }
+}

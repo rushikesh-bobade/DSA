102fb9d43d3ff078fdbd291f60958b037f657402
Rushikesh Bobade <155564428+rushikesh-bobade@users.noreply.github.com>
2026-06-07 19:57:32 +0530

    ✅ Maximum Subarray | Medium | Java | Runtime: 0 ms | Memory: 42.5 MB | 2026-06-07

--- medium/0053-maximum-subarray/solution.java
+++ medium/0053-maximum-subarray/solution.java
@@ -2,5 +2,15 @@ class Solution {
     public int maxSubArray(int[] nums) {
         int maxSum=0;
         int count=0;
+
+        for(int i =0;i<nums.length;i++){
+            count=count+nums[i];
+            maxSum=Math.max(maxSum,count);
+            if(count<0){
+                count=0;
+            }
+        }
+    return maxSum;
     }
+    
 }

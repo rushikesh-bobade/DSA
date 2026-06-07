7f6b6b8262cf5f9e435aa12257b9b5f658e149ec
Rushikesh Bobade <155564428+rushikesh-bobade@users.noreply.github.com>
2026-06-07 20:36:17 +0530

    ✅ Jump Game | Medium | Java | Runtime: 2 ms (88.04%) | Memory: 47.9 MB | 2026-06-07

--- medium/0055-jump-game/solution.java
+++ medium/0055-jump-game/solution.java
@@ -1,13 +1,10 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        int maxIndex=0;
-
-        for(int i=0;i<nums.length;i++){
-            
-            if(i>maxIndex){
-                return false;
-            }
-            maxIndex=Math.max(maxIndex,i+nums[i]);
+        int maxReach = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (i > maxReach) return false;
+            maxReach = Math.max(maxReach, i + nums[i]);
+            if (maxReach >= nums.length - 1) return true;
         }
         return true;
     }

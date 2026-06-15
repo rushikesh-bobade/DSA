5789ecf7334a0923dcd0549ff60d59b380c85860
Rushikesh Bobade <155564428+rushikesh-bobade@users.noreply.github.com>
2026-06-16 02:22:34 +0530

    ✅ Search Insert Position | Easy | Java | Runtime: 0 ms (100.00%) | Memory: 44.5 MB | 2026-06-15

--- easy/0035-search-insert-position/solution.java
+++ easy/0035-search-insert-position/solution.java
@@ -1,11 +1,20 @@
-                return mid;
-            if(nums[mid]==target){
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int s=0;
+        int e=nums.length-1;
 
-             int mid=(s+e)/2;
-        while(s<=e){
 
-            }else if(nums[mid]<target){
-                s=mid+1;
-            }else{
-                e=mid-1;
-            }
+        while(s<=e){
+             int mid=(s+e)/2;
+
+            if(nums[mid]==target){
+                return mid;
+            }else if(nums[mid]<target){
+                s=mid+1;
+            }else{
+                e=mid-1;
+            }
+        }
+        return s;
+    }
+}

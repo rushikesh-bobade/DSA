092ce8a78f3a122f2438465d4b4fcfc80f584cce
Rushikesh Bobade <155564428+rushikesh-bobade@users.noreply.github.com>
2026-06-16 02:12:49 +0530

    ✅ Search Insert Position | Easy | Java | Runtime: 0 ms | Memory: 42.4 MB | 2026-06-15

--- easy/0035-search-insert-position/solution.java
+++ easy/0035-search-insert-position/solution.java
@@ -1,10 +1,12 @@
-            }else{
-                s=mid+1;
-            }else if(nums[mid]<target){
                 return mid;
+            }else if(nums[mid]<target){
+                s=mid+1;
+            }else{
                 e=mid-1;
             }
         }
         return -1;
     }
-}
+            if(nums[mid]==target){
+
+             int mid=s+(e-s)/2;

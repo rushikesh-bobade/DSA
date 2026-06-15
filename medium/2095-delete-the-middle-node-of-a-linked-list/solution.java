0d00536206c0913d17303cadb485e65baad70041
Rushikesh Bobade <155564428+rushikesh-bobade@users.noreply.github.com>
2026-06-16 02:06:27 +0530

    ✅ Delete the Middle Node of a Linked List | Medium | Java | Runtime: 0 ms | Memory: 42.5 MB | 2026-06-15

--- medium/2095-delete-the-middle-node-of-a-linked-list/solution.java
+++ medium/2095-delete-the-middle-node-of-a-linked-list/solution.java
@@ -1,12 +1,1 @@
-        return head;
-       }
-
-        ListNode slow=head;
-        ListNode fast=head;
-        ListNode prev=head;
-
-        while(fast!=null && fast.next==null){
-            prev=slow;
-            slow=slow.next;
-            fast=fast.next.next;
-        }
+ 

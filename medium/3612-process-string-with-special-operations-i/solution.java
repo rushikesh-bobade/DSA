6e561a1130fda328aea2b5d45319f624a1edf83d
Rushikesh Bobade <155564428+rushikesh-bobade@users.noreply.github.com>
2026-06-16 21:14:13 +0530

    ✅ Process String with Special Operations I | Medium | Java | Runtime: 0 ms | Memory: 42.5 MB | 2026-06-16

--- medium/3612-process-string-with-special-operations-i/solution.java
+++ medium/3612-process-string-with-special-operations-i/solution.java
@@ -1,13 +1,5 @@
-            if(ch=='#'){
-                result.append(result);
-            }else if(ch=='%'){
                 result.reverse();
-
-            char ch = s.charAt(i);
- 
             }else if(ch=='*'){
                 if(result.length()>0){
-                    result.deleteCharAt(-1);
+                    result.deleteCharAt(result.length()-1);
                 }
-            }else{
-                result.append(result);

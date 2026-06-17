0bb0c60097a595d08a05ceaa88fc00684276e712
Rushikesh Bobade <155564428+rushikesh-bobade@users.noreply.github.com>
2026-06-18 00:05:09 +0530

    ✅ Process String with Special Operations II | Hard | Java | Runtime: 32 ms (26.92%) | Memory: 47.7 MB | 2026-06-17

--- hard/3614-process-string-with-special-operations-ii/solution.java
+++ hard/3614-process-string-with-special-operations-ii/solution.java
@@ -1,14 +1,57 @@
+class Solution {
+    public char processStr(String s, long k) {
 
-            } else if (ch == '%') {
+        int n = s.length();
+        long[] len = new long[n + 1];
 
-                long curLen = len[i + 1];
-                k = curLen - 1 - k;
+        for (int i = 0; i < n; i++) {
+            char ch = s.charAt(i);
 
-            } else { // '*'
-                // nothing to do
-            }
-        }
+            if (ch >= 'a' && ch <= 'z') {
+                len[i + 1] = len[i] + 1;
+            } else if (ch == '*') {
+                len[i + 1] = Math.max(0, len[i] - 1);
+            } else if (ch == '#') {
+                len[i + 1] = len[i] * 2;
+            } else { // %
+                len[i + 1] = len[i];
+            }
+        }
 
-        return '.';
-    }
+        long finalLen = len[n];
+
+        if (k >= finalLen) {
+            return '.';
+        }
+
+        for (int i = n - 1; i >= 0; i--) {
+
+            char ch = s.charAt(i);
+
+            if (ch >= 'a' && ch <= 'z') {
+
+                if (k == len[i + 1] - 1) {
+                    return ch;
+                }
+
+            } else if (ch == '#') {
+
+                long oldLen = len[i];
+
+                if (k >= oldLen) {
+                    k -= oldLen;
+                }
+
+            } else if (ch == '%') {
+
+                long curLen = len[i + 1];
+                k = curLen - 1 - k;
+
+            } else { // '*'
+                // nothing to do
+            }
+        }
+
+        return '.';
+    }
 }

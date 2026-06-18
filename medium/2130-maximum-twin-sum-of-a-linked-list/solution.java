992ea5d0db475df26fa327beb6e1d4e7c9e9113a
Rushikesh Bobade <155564428+rushikesh-bobade@users.noreply.github.com>
2026-06-18 12:13:39 +0530

    ✅ Maximum Twin Sum of a Linked List | Medium | Java | Runtime: 0 ms | Memory: 42.7 MB | 2026-06-18

---
+++ medium/2130-maximum-twin-sum-of-a-linked-list/solution.java
@@ -0,0 +1,14 @@
+        // Calculate maximum twin sum
+        int maxSum = 0;
+        ListNode first = head;
+        ListNode second = prev;
+
+        while (second != null) {
+            maxSum = Math.max(maxSum, first.val + second.val);
+            first = first.next;
+            second = second.next;
+        }
+
+        return maxSum;
+    }
+}
